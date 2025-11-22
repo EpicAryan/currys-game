b21ff1fe13b11245d74befd4f8128a986624adba
EpicAryan <adaryan0811@gmail.com>
2025-11-22 21:40:39 +0530

    feat: add plausible analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
+import { init } from "@plausible-analytics/tracker";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,10 @@ export const metadata: Metadata = {
   description: "Curry Advent Campaign",
 };
 
+init({
+  domain: process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN!, // Your production domain
+  captureOnLocalhost: process.env.NODE_ENV === 'development', // Enable on localhost
+});
 export default function RootLayout({
   children,
 }: Readonly<{
